ad6e65b6f3f5ffacaa604bb753f8a3a72801f2f0
Phil Duffen <phil@duffen.co.uk>
2018-11-13 15:58:06 +0000

    Add cookie values to index spec

--- spec/views/challenges/index.html.erb_spec.rb
+++ spec/views/challenges/index.html.erb_spec.rb
@@ -1,9 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe "challenges/index.html.erb", type: :view do
+
+	before(:each) do
+      @request.cookies[:telephone_number] = "447585601232"  # This line is crucial!
+      @request.cookies[:code] = '1234'
+    end
   it 'allows user to input mobile number' do
     visit root_path
-    fill_in 'challenge_phone_number', with: '074545601232'
-    visit challenges_code_confirm_path
+    fill_in 'challenge_phone_number', with: '07585601232'
+  end
+
+  it 'allows user to input code' do
   end
 end
