2e80a2955496f2f4e3273e149b8dd7d25d3a2085
Phil Duffen <phil@duffen.co.uk>
2018-11-15 10:16:40 +0000

    Add new service for preparing and sending messages (codes).

---
+++ app/services/message_service.rb
@@ -0,0 +1,44 @@
+class MessageService
+
+  attr_accessor :phone_number, :message
+
+  def initialize(phone_number, message=nil)
+    @phone_number = phone_number
+    @message = message || generate_code
+  end
+
+  def send_message
+    ZensendService.send_sms(@message, prepare_for_sending)
+  end
+
+  def valid_number?
+    !!(@phone_number =~ /^(?:0|44)(\d+)$/)
+  end
+
+  private
+
+  def format_number(phone_number)
+    if @phone_number[0] == '0'
+      @phone_number.slice!(0)
+      @phone_number.prepend('44')
+    end
+    @phone_number
+  end
+
+  def generate_code
+    rand.to_s[2..5]
+  end
+  
+  def prepare_for_sending
+    return unless valid_number?
+    format_number(phone_number)
+  end
+end
+
+
+# phone_number = params[:challenge][:phone_number]
+# (redirect_to root_path and return) unless valid_number?(phone_number)
+# phone_number = format_number(phone_number)
+# code = generate_code
+# store_values(phone_number, code)
+# ZensendService.send_sms(code, phone_number)
